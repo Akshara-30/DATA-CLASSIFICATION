fe40bd89a2192d95a8583237c28445a7c2a96fc2
Akshara-30 <66627698+Akshara-30@users.noreply.github.com>
2024-12-05 17:11:25 +0530

    main branch file added

---
+++ CKMuxConnectorHubService.java
@@ -0,0 +1,10 @@
+package com.lti.knowledge.service;
+
+import java.util.List;
+
+import com.lti.knowledge.to.ConnectorHubTO;
+
+public interface CKMuxConnectorHubService {
+	List<ConnectorHubTO> getConnectors();
+
+}
